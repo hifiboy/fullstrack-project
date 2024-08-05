6efcaf275eda0f905dd5381fc1bead0b3fa11200
Balamurugan.k <125335506+hifiboy@users.noreply.github.com>
2024-08-05 09:42:16 +0530

    Delete staffscheduling directory

--- staffscheduling/src/main/java/com/staffscheduling/staffscheduling/Service/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.staffscheduling.staffscheduling.Service;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
